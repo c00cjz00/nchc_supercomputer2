bee637ca9be965fc922ab27c94af4e9e452b48b1
c00cjz00 <summerhill001@gmail.com>
2018-10-23 09:45:37 +0800

    init

--- init.sh
+++ init.sh
@@ -9,6 +9,11 @@ git config --global user.name "c00cjz00"
 git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
+git rm 0-autoSSH.php
+git rm 1-autoSSH_sendCmd.php
+git rm 2-autoSSH_scp.php
+git rm 3-autoSSH_sftp.php
+
 git add *
 git commit -m "init"
 # 上傳至遠端
